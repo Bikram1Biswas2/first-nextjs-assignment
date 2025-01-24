2343213b86a574ca0891f21b5113c88619e86c99
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-24 16:32:17 +0530

    make the profile page private route

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,25 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+const Profile = async () => {
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
 
-const Profile = () => {
-    return (
-        <div>
-           <h2>Welcome to your profile!</h2>
-        </div>
-    );
+  if (!user) {
+    redirect('/api/auth/login');
+  }
+
+ 
+  return (
+    <div className="w-11/12 mx-auto mt-10 text-center">
+      <h2 className="text-3xl font-bold">Welcome to your profile!</h2>
+      <p className="mt-4 text-lg text-gray-600">
+        You are now logged in as <span className="font-semibold">{user.email || user.name}</span>.
+      </p>
+    </div>
+  );
 };
 
 export default Profile;
